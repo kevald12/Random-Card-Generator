ca8fef9726c509b8d598ead574aad3a1fa079a57
kevald12 <kealable@gmail.com>
2022-09-04 19:00:26 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRamdonPalo());
+  document.querySelector(".card").innerHTML = generateRamdonCardNumber();
+};
+
+let generateRamdonCardNumber = () => {
+  let nros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * nros.length);
+  return nros[randomNumber];
+};
+
+let generateRamdonPalo = () => {
+  let palos = ["diamonds", "hearts", "clubs", "spades"];
+  let randomPalo = Math.floor(Math.random() * palos.length);
+  return palos[randomPalo];
 };
